d6dcfc32795c270e0f86b45879ef1ab9caecee36
Prashant Kumar <prashant.aman9@gmail.com>
2023-11-27 13:39:56 +0530

    Add delete label api

--- controllers/api/v1/label.js
+++ controllers/api/v1/label.js
@@ -2,6 +2,7 @@
 /* eslint-disable import/prefer-default-export */
 
 import Label from '../../../models/label.js'
+import Post from '../../../models/post.js'
 
 /* CREATE LABEL- requires admin authentication */
 export const createLabel = async (req, res) => {
@@ -35,6 +36,48 @@ export const createLabel = async (req, res) => {
       data: {
         label,
       },
+    })
+  } catch (err) {
+    console.log(err)
+    return res.status(500).json({
+      success: false,
+      message: 'Internal Server Error',
+    })
+  }
+}
+
+/* DELETE LABEL- requires admin authentication */
+
+export const deleteLabel = async (req, res) => {
+  try {
+    const { labelId } = req.params
+    const { user } = req
+    if (!user.isAdmin) {
+      return res.status(422).json({
+        success: false,
+        message: 'You are not an admin to delete label',
+      })
+    }
+
+    const label = await Label.findById(labelId)
+    if (!label) {
+      return res.status(422).json({
+        success: false,
+        message: "Label doesn't exist",
+      })
+    }
+    // Iterate on posts array and pull/remove labelid from that post
+    label.posts.forEach(async (postId) => {
+      await Post.findByIdAndUpdate(postId, {
+        $pull: { labels: labelId },
+      })
+    })
+
+    await Label.findByIdAndDelete(labelId)
+
+    return res.status(200).json({
+      success: true,
+      message: 'Label deleted successfully',
     })
   } catch (err) {
     console.log(err)
--- routes/api/v1/label.js
+++ routes/api/v1/label.js
@@ -1,11 +1,13 @@
 import express from 'express'
 import passport from 'passport'
 import '../../../config/passport-jwt-strategy.js'
-import { createLabel } from '../../../controllers/api/v1/label.js'
+import { createLabel, deleteLabel } from '../../../controllers/api/v1/label.js'
 
 const router = express.Router()
 
 /* /api/v1/labels/create */
 router.post('/create', passport.authenticate('jwt', { session: false }), createLabel)
+/* /api/v1/labels/delete/:labelId */
+router.delete('/delete/:labelId', passport.authenticate('jwt', { session: false }), deleteLabel)
 
 export default router
